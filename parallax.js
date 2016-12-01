5c34e9f3345aa9e8c9da707f2344ddd9e834723a
vandoan <vandoona@gmail.com>
2016-11-30 22:54:03 -0500

    Parallax - simple formula

--- parallax.js
+++ parallax.js
@@ -1,4 +1,22 @@
 
+
+ // if image card is inside viewport 
+if( $('#image-card').offset().top < $(window).scrollTop() + $(window).height() && $('#image-card').offset().top + $('#image-card').height() > $(window).scrollTop() ){
+	// upon scroll up, add px to 
+
+	// upon scroll down, reduce px 
+
+}
+
+
+
+// parallax formula
+// ( #imageContainer - windowTop ) / - speed
+( $('#image-card').offset().top - $(window).scrollTop() ) / 2
+
+
+
+
 function parallax( image, speed ){
 	$(image).css('top', ( ( $('#image-card').offset().top - $(window).scrollTop() ) / -speed ) +'px' );
 }
