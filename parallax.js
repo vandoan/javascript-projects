cf750de53d717cf5d7a06dbf2e50b3183be88ece
vandoan <vandoona@gmail.com>
2016-12-01 23:51:23 -0500

    Parallax - update formula

--- parallax.js
+++ parallax.js
@@ -13,14 +13,53 @@ if( $('#image-card').offset().top < $(window).scrollTop() + $(window).height() &
 // parallax formula
 // ( #imageContainer - windowTop ) / - speed
 ( $('#image-card').offset().top - $(window).scrollTop() ) / 2
+var imageCardTop = $('#image-card').offset().top;
 
 
+// parallax formula
+// ( #imageContainer - windowTop ) / - speed
+( $('#image-card').offset().top - $(window).scrollTop() ) / 2
+var imageCardTop = $('#image-card').offset().top;
 
-
-function parallax( image, speed ){
-	$(image).css('top', ( ( $('#image-card').offset().top - $(window).scrollTop() ) / -speed ) +'px' );
+function parallax( image, speed, finalTop ){
+	if( $('#image-card').offset().top < $(window).scrollTop() + $(window).height() && $('#image-card').offset().top + $('#image-card').height() > $(window).scrollTop() ){
+		$(image).css('top', ( ( imageCardTop*(finalTop*speed)/imageCardTop) - $(window).scrollTop() ) / speed ) +'px' );
+	}
 }
+
+
+
+
+
+
+
+var imageCardTop = $('#image-card').offset().top;
+
+
+
+function parallax(){
+	var windowTop = $(window).scrollTop();
+	$('.parallax').each(function(){
+		var top = $(this).data('top');
+		var speed = $(this).data('speed');
+ 		if( $('#image-card').offset().top < $(window).scrollTop() + $(window).height() && $('#image-card').offset().top + $('#image-card').height() > $(window).scrollTop() ){
+		 	$(this).css('top', ( top + (imageCardTop-windowTop) * (1 / speed ) +'%' ) );
+		}
+	});
+}
+
+ 
+
+// function parallax( image, speed ){
+// 	if( $('#image-card').offset().top < $(window).scrollTop() + $(window).height() && $('#image-card').offset().top + $('#image-card').height() > $(window).scrollTop() ){
+// 		$(image).css('top', ( ( $('#image-card').offset().top - $(window).scrollTop() ) / speed ) +'px' );
+// 	}
+// }
+ 
+
 
 $(window).scroll(function(){
 	parallax();
 });
+
+
